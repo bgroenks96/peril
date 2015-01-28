2f8d6f66e436bf49b292f8d73ad1ac95bd4568a1
Aaron Mahan <aaron@forerunnergames.com>
2015-01-28 05:02:23 -0900

    Make Play screen the starting screen for now.

--- client/src/main/java/com/forerunnergames/peril/client/settings/ScreenSettings.java
+++ client/src/main/java/com/forerunnergames/peril/client/settings/ScreenSettings.java
@@ -6,7 +6,7 @@ import com.forerunnergames.tools.common.Classes;
 public final class ScreenSettings
 {
   // @formatter:off
-  public static final ScreenId START_SCREEN = ScreenId.MAIN_MENU;
+  public static final ScreenId START_SCREEN = ScreenId.PLAY;
   // @formatter:on
 
   private ScreenSettings ()
