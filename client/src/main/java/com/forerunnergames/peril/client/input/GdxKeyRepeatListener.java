10f1ed07623e8cf07a0de3d5fc0f2f7177e4249c
Aaron Mahan <aaron@forerunnergames.com>
2015-04-08 00:51:12 -0800

    Add support for repeating non-printable keys.
    LibGDX doesn't support repeating non-printable keys (such as arrow keys)
    by default. Add a key repeat system that allows for setting the initial
    repeat delay, and subsequent repeat rate individually for every
    non-printable key in LibGDX. Create a callback interface that will be
    called every time a repeating key repeats, and once when it stop
    repeating (key is released).

---
+++ client/src/main/java/com/forerunnergames/peril/client/input/GdxKeyRepeatListener.java
@@ -0,0 +1,8 @@
+package com.forerunnergames.peril.client.input;
+
+public interface GdxKeyRepeatListener
+{
+  void keyUp (final int keyCode);
+
+  void keyDownRepeating (final int keyCode);
+}
---
+++ client/src/main/java/com/forerunnergames/peril/client/input/GdxKeyRepeatListenerAdapter.java
@@ -0,0 +1,14 @@
+package com.forerunnergames.peril.client.input;
+
+public class GdxKeyRepeatListenerAdapter implements GdxKeyRepeatListener
+{
+  @Override
+  public void keyUp (final int keyCode)
+  {
+  }
+
+  @Override
+  public void keyDownRepeating (final int keyCode)
+  {
+  }
+}
