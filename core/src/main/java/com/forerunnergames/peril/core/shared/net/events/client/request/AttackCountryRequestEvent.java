4f3f08ba269e637b70cc83b5affbdbb3d2c82cbf
Aaron Mahan <aaron@forerunnergames.com>
2015-07-20 01:00:10 -0800

    Add missing default event ctors for kryonet registration.

--- core/src/main/java/com/forerunnergames/peril/core/shared/net/events/client/request/AttackCountryRequestEvent.java
+++ core/src/main/java/com/forerunnergames/peril/core/shared/net/events/client/request/AttackCountryRequestEvent.java
@@ -1,7 +1,12 @@
 package com.forerunnergames.peril.core.shared.net.events.client.request;
 
+import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
 import com.forerunnergames.tools.net.events.remote.origin.client.ClientRequestEvent;
 
 public final class AttackCountryRequestEvent implements ClientRequestEvent
 {
+  @RequiredForNetworkSerialization
+  public AttackCountryRequestEvent ()
+  {
+  }
 }
--- core/src/main/java/com/forerunnergames/peril/core/shared/net/events/client/request/DefendCountryRequestEvent.java
+++ core/src/main/java/com/forerunnergames/peril/core/shared/net/events/client/request/DefendCountryRequestEvent.java
@@ -1,7 +1,12 @@
 package com.forerunnergames.peril.core.shared.net.events.client.request;
 
+import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
 import com.forerunnergames.tools.net.events.remote.origin.client.ClientRequestEvent;
 
 public final class DefendCountryRequestEvent implements ClientRequestEvent
 {
+  @RequiredForNetworkSerialization
+  public DefendCountryRequestEvent ()
+  {
+  }
 }
