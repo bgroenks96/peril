9d10e005969a2d42395793b3a127d908816cad5f
Aaron Mahan <aaron@forerunnergames.com>
2015-07-25 18:20:25 -0800

    Move JoinGameHandler into its own class.
    JoinGameHandler needs to be shared by both Create/Join Multiplayer Game:
    Classic Mode screens.
    
    PERIL-214 #Done

--- client/src/main/java/com/forerunnergames/peril/client/ui/screens/menus/multiplayer/modes/classic/joingame/MultiplayerClassicGameModeJoinGameMenuScreen.java
+++ client/src/main/java/com/forerunnergames/peril/client/ui/screens/menus/multiplayer/modes/classic/joingame/MultiplayerClassicGameModeJoinGameMenuScreen.java
@@ -1,11 +1,5 @@
 package com.forerunnergames.peril.client.ui.screens.menus.multiplayer.modes.classic.joingame;
 
-import static com.forerunnergames.peril.core.shared.net.events.EventFluency.clientConfigurationFrom;
-import static com.forerunnergames.peril.core.shared.net.events.EventFluency.gameServerConfigurationFrom;
-import static com.forerunnergames.peril.core.shared.net.events.EventFluency.playerFrom;
-import static com.forerunnergames.peril.core.shared.net.events.EventFluency.playerNameFrom;
-import static com.forerunnergames.peril.core.shared.net.events.EventFluency.playersInGameFrom;
-
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -18,38 +12,19 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 
-import com.forerunnergames.peril.client.events.JoinGameEvent;
 import com.forerunnergames.peril.client.ui.screens.ScreenChanger;
 import com.forerunnergames.peril.client.ui.screens.ScreenId;
 import com.forerunnergames.peril.client.ui.screens.ScreenSize;
 import com.forerunnergames.peril.client.ui.screens.menus.AbstractMenuScreen;
 import com.forerunnergames.peril.client.ui.screens.menus.MenuScreenWidgetFactory;
-import com.forerunnergames.peril.core.model.people.person.PersonIdentity;
+import com.forerunnergames.peril.client.ui.screens.menus.multiplayer.modes.classic.shared.JoinGameHandler;
 import com.forerunnergames.peril.core.model.settings.GameSettings;
-import com.forerunnergames.peril.core.shared.net.GameServerConfiguration;
-import com.forerunnergames.peril.core.shared.net.events.client.request.JoinGameServerRequestEvent;
-import com.forerunnergames.peril.core.shared.net.events.client.request.PlayerJoinGameRequestEvent;
-import com.forerunnergames.peril.core.shared.net.events.server.denied.JoinGameServerDeniedEvent;
-import com.forerunnergames.peril.core.shared.net.events.server.denied.PlayerJoinGameDeniedEvent;
-import com.forerunnergames.peril.core.shared.net.events.server.success.JoinGameServerSuccessEvent;
-import com.forerunnergames.peril.core.shared.net.events.server.success.PlayerJoinGameSuccessEvent;
-import com.forerunnergames.peril.core.shared.net.packets.person.PlayerPacket;
 import com.forerunnergames.peril.core.shared.net.settings.NetworkSettings;
-import com.forerunnergames.tools.common.Arguments;
 import com.forerunnergames.tools.common.Event;
-import com.forerunnergames.tools.net.client.ClientConfiguration;
-import com.forerunnergames.tools.net.client.UnknownClientConfiguration;
 import com.forerunnergames.tools.net.server.DefaultServerConfiguration;
 import com.forerunnergames.tools.net.server.ServerConfiguration;
 
-import com.google.common.collect.ImmutableSet;
-
-import javax.annotation.Nullable;
-
 import net.engio.mbassy.bus.MBassador;
-import net.engio.mbassy.listener.Handler;
-import net.engio.mbassy.listener.Listener;
-import net.engio.mbassy.listener.References;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -105,11 +80,11 @@ public final class MultiplayerClassicGameModeJoinGameMenuScreen extends Abstract
     final Table playerSettingsTable = new Table ().top ().left ();
     playerSettingsTable.add ().height (23).colspan (5);
     playerSettingsTable.row ();
-    playerSettingsTable.add (widgetFactory.createMenuSettingSectionTitleText ("Your Player")).size (538, 42).fill ().padLeft (60)
-            .padRight (60).left ().colspan (5);
+    playerSettingsTable.add (widgetFactory.createMenuSettingSectionTitleText ("Your Player")).size (538, 42).fill ()
+            .padLeft (60).padRight (60).left ().colspan (5);
     playerSettingsTable.row ();
-    playerSettingsTable.add (widgetFactory.createMenuSettingText ("Name")).size (150, 40).fill ().padLeft (90)
-            .left ().spaceRight (10);
+    playerSettingsTable.add (widgetFactory.createMenuSettingText ("Name")).size (150, 40).fill ().padLeft (90).left ()
+            .spaceRight (10);
     playerSettingsTable.add (playerNameTextField).size (204, 28).fill ().left ().colspan (3).spaceLeft (10);
     playerSettingsTable.add ().expandX ().fill ();
     playerSettingsTable.row ();
@@ -125,11 +100,11 @@ public final class MultiplayerClassicGameModeJoinGameMenuScreen extends Abstract
     gameSettingsTable.row ();
     gameSettingsTable.add ().height (18).colspan (3);
     gameSettingsTable.row ();
-    gameSettingsTable.add (widgetFactory.createMenuSettingSectionTitleText ("Game Settings")).size (538, 42).fill ().padLeft (60)
-            .padRight (60).left ().colspan (3);
+    gameSettingsTable.add (widgetFactory.createMenuSettingSectionTitleText ("Game Settings")).size (538, 42).fill ()
+            .padLeft (60).padRight (60).left ().colspan (3);
     gameSettingsTable.row ();
-    gameSettingsTable.add (widgetFactory.createMenuSettingText ("Server Address")).size (150, 40).fill ()
-            .padLeft (90).left ().spaceRight (10);
+    gameSettingsTable.add (widgetFactory.createMenuSettingText ("Server Address")).size (150, 40).fill ().padLeft (90)
+            .left ().spaceRight (10);
     gameSettingsTable.add (serverAddressTextField).size (204, 28).fill ().left ().spaceLeft (10);
     gameSettingsTable.add ().expandX ().fill ();
     verticalGroup.addActor (gameSettingsTable);
@@ -160,16 +135,15 @@ public final class MultiplayerClassicGameModeJoinGameMenuScreen extends Abstract
         final String rawPlayerName = playerNameTextField.getText ();
         final String rawPlayerClanTag = playerClanTagTextField.getText ();
         final String finalPlayerName = rawPlayerClanTag.isEmpty () ? rawPlayerName
-                : GameSettings.PLAYER_CLAN_TAG_START_SYMBOL + rawPlayerClanTag
-                        + GameSettings.PLAYER_CLAN_TAG_END_SYMBOL + " " + rawPlayerName;
+                : GameSettings.PLAYER_CLAN_TAG_START_SYMBOL + rawPlayerClanTag + GameSettings.PLAYER_CLAN_TAG_END_SYMBOL
+                        + " " + rawPlayerName;
         final String finalServerAddress = serverAddressTextField.getText ();
+        final ServerConfiguration serverConfig = new DefaultServerConfiguration (finalServerAddress,
+                NetworkSettings.DEFAULT_TCP_PORT);
 
         // TODO Go to loading screen.
 
-        final JoinGameHandler joinGameHandler = new JoinGameHandler (finalPlayerName, finalServerAddress,
-                NetworkSettings.DEFAULT_TCP_PORT);
-
-        joinGameHandler.joinGame ();
+        new JoinGameHandler (finalPlayerName, serverConfig, screenChanger, eventBus).joinGame ();
       }
     });
   }
@@ -193,126 +167,5 @@ public final class MultiplayerClassicGameModeJoinGameMenuScreen extends Abstract
         toScreen (ScreenId.MULTIPLAYER_CLASSIC_GAME_MODE_MENU);
       }
     });
-  }
-
-  @Listener (references = References.Strong)
-  private final class JoinGameHandler
-  {
-    private final String playerName;
-    private final ServerConfiguration serverConfig;
-    private final ImmutableSet.Builder <PlayerPacket> playersBuilder = ImmutableSet.builder ();
-    @Nullable
-    private GameServerConfiguration gameServerConfig = null;
-    private ClientConfiguration clientConfig = new UnknownClientConfiguration ();
-
-    public void joinGame ()
-    {
-      eventBus.subscribe (this);
-
-      final JoinGameServerRequestEvent event = new JoinGameServerRequestEvent (serverConfig);
-
-      log.info ("Attempting to join game server... [{}]", event);
-
-      eventBus.publishAsync (event);
-    }
-
-    @Handler
-    public void onJoinGameServerSuccessEvent (final JoinGameServerSuccessEvent event)
-    {
-      Arguments.checkIsNotNull (event, "event");
-
-      log.debug ("Event received [{}]", event);
-      log.info ("Successfully joined game server [{}]", event);
-
-      // TODO Loading screen progress update
-
-      playersBuilder.addAll (playersInGameFrom (event));
-      gameServerConfig = gameServerConfigurationFrom (event);
-      clientConfig = clientConfigurationFrom (event);
-
-      final PlayerJoinGameRequestEvent playerJoinGameRequestEvent = new PlayerJoinGameRequestEvent (playerName);
-
-      log.info ("Attempting to join game as a player... [{}]", playerJoinGameRequestEvent);
-
-      eventBus.publishAsync (playerJoinGameRequestEvent);
-    }
-
-    @Handler
-    public void onJoinGameServerDeniedEvent (final JoinGameServerDeniedEvent event)
-    {
-      Arguments.checkIsNotNull (event, "event");
-
-      log.debug ("Event received [{}]", event);
-      log.error ("Could not join game server: [{}]", event);
-
-      // TODO Error popup
-
-      eventBus.unsubscribe (this);
-    }
-
-    @Handler
-    public void onPlayerJoinGameSuccessEvent (final PlayerJoinGameSuccessEvent event)
-    {
-      Arguments.checkIsNotNull (event, "event");
-
-      log.debug ("Event received [{}]", event);
-
-      if (event.getPlayer ().has (PersonIdentity.NON_SELF))
-      {
-        log.warn ("Received {} with {} while expecting {}", event, PersonIdentity.NON_SELF, PersonIdentity.SELF);
-        return;
-      }
-
-      playersBuilder.add (playerFrom (event));
-
-      log.info ("Successfully joined game as a player: [{}]", event);
-
-      // TODO Loading screen progress update
-
-      // Go to the play screen
-      // When toScreen returns, the play screen will be subscribed
-      toScreen (ScreenId.PLAY_CLASSIC);
-
-      // Don't unsubscribe JoinGameHandler until we're already subscribed on the play screen.
-      eventBus.unsubscribe (this);
-
-      // This should be impossible because it is set in JoinGameServerSuccessEvent handler,
-      // which is a prerequisite to arriving here.
-      assert gameServerConfig != null;
-
-      // Forward the data from JoinGameServerSuccessEvent & PlayerJoinGameSuccessEvent to the play screen.
-      eventBus.publishAsync (new JoinGameEvent (playersBuilder.build (), gameServerConfig, clientConfig));
-    }
-
-    @Handler
-    public void onPlayerJoinGameDeniedEvent (final PlayerJoinGameDeniedEvent event)
-    {
-      Arguments.checkIsNotNull (event, "event");
-
-      log.debug ("Event received [{}]", event);
-
-      if (!playerNameFrom (event).equals (playerName))
-      {
-        log.warn ("Received [{}] with player name [{}] while expecting player name [{}]", event,
-                  playerNameFrom (event), playerName);
-        return;
-      }
-
-      log.error ("Could not join game as a player: [{}]", event);
-
-      // TODO Error popup
-
-      eventBus.unsubscribe (this);
-    }
-
-    private JoinGameHandler (final String playerName, final String serverAddress, final int tcpPort)
-    {
-      Arguments.checkIsNotNull (playerName, "playerName");
-      Arguments.checkIsNotNull (serverAddress, "serverAddresss");
-      Arguments.checkIsNotNegative (tcpPort, "tcpPort");
-
-      this.playerName = playerName;
-      serverConfig = new DefaultServerConfiguration (serverAddress, tcpPort);
-    }
   }
 }
