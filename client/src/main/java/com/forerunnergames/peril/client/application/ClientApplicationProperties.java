bgroenks96/peril

commit be7feca6b09be1a4bd3e1a0d38e20f8c2ea8f5bb

Fix Windows path bug in settings.txt.
In settings.txt, the updated-assets-directory setting requires a local
absolute path, that may or may not contain os-specific path separators.

This causes problems however on Windows, where os-specific paths have
single backslashes.

The settings file is loaded as a java Properties file, which reserves
the single backslash character for continuing lines (soft line breaks).

When loading settings.txt with a single backslash Windows path, the
backslashes are erased, causing the path to not be recognized as a valid
directory.

The workaround is to load settings.txt as a String first, replace any
single backslashes with double backslashes, and then load it as a
Properties file.

This allows Windows users to be able to copy and paste native paths
straight into the settings file without modifications.

client/src/main/java/com/forerunnergames/peril/client/application/ClientApplicationProperties.java
@@ -7,11 +7,13 @@ import com.forerunnergames.peril.client.settings.ScreenSettings;
 import com.forerunnergames.peril.client.ui.screens.ScreenId;
 import com.forerunnergames.tools.common.LetterCase;
 import com.forerunnergames.tools.common.Strings;
+import com.forerunnergames.tools.common.io.FileUtils;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -102,11 +104,15 @@ public final class ClientApplicationProperties
     defaults.setProperty (UPDATE_ASSETS_KEY, String.valueOf (AssetSettings.UPDATE_ASSETS));
     defaults.setProperty (UPDATED_ASSETS_DIRECTORY_KEY, AssetSettings.ABSOLUTE_UPDATED_ASSETS_DIRECTORY);
 
+    final String propertiesFileContents = FileUtils.loadFile (PROPERTIES_FILE_PATH_AND_NAME, StandardCharsets.UTF_8);
+
     properties = new Properties (defaults);
 
     try
     {
-      properties.load (new FileInputStream (PROPERTIES_FILE_PATH_AND_NAME));
+      // Deal with Windows path single backslashes being erased because it's a reserved character for continuing lines
+      // by the Properties class.
+      properties.load (new StringReader (propertiesFileContents.replace ("\\", "\\\\")));
     }
     catch (final IOException e)
     {

commit ceef7489d24060646bc97f27d271bdaa28fbcd9e

Fix settings.txt loading / creating bug.
This is a regression introduced by
be7feca6b09be1a4bd3e1a0d38e20f8c2ea8f5bb:

Commit message:

"Fix Windows path bug in settings.txt.

In settings.txt, the updated-assets-directory setting requires a local
absolute path, that may or may not contain os-specific path separators.

This causes problems however on Windows, where os-specific paths have
single backslashes.

The settings file is loaded as a java Properties file, which reserves
the single backslash character for continuing lines (soft line breaks).

When loading settings.txt with a single backslash Windows path, the
backslashes are erased, causing the path to not be recognized as a valid
directory.

The workaround is to load settings.txt as a String first, replace any
single backslashes with double backslashes, and then load it as a
Properties file.

This allows Windows users to be able to copy and paste native paths
straight into the settings file without modifications."

- The loading of the properties file into a String was placed outside of
  the try/catch block set up for the actual call to Properties#load,
  where in the catch block, settings.txt will be created if it does not
  exist. This situation results in a NoSuchFileException when attempting
  to load the properties file into a String.

- The solution is to place the loading of the properties file into a
  String inside the same try/catch that contains the Properties#load
  call. This way, settings.txt will still be created even when attempting
  to load it into a String.

client/src/main/java/com/forerunnergames/peril/client/application/ClientApplicationProperties.java
@@ -104,12 +104,12 @@ public final class ClientApplicationProperties
     defaults.setProperty (UPDATE_ASSETS_KEY, String.valueOf (AssetSettings.UPDATE_ASSETS));
     defaults.setProperty (UPDATED_ASSETS_DIRECTORY_KEY, AssetSettings.ABSOLUTE_UPDATED_ASSETS_DIRECTORY);
 
-    final String propertiesFileContents = FileUtils.loadFile (PROPERTIES_FILE_PATH_AND_NAME, StandardCharsets.UTF_8);
-
     properties = new Properties (defaults);
 
     try
     {
+      final String propertiesFileContents = FileUtils.loadFile (PROPERTIES_FILE_PATH_AND_NAME, StandardCharsets.UTF_8);
+
       // Deal with Windows path single backslashes being erased because it's a reserved character for continuing lines
       // by the Properties class.
       properties.load (new StringReader (propertiesFileContents.replace ("\\", "\\\\")));