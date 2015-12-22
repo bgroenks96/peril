a7482800f09d2441599544d654b1cdee5739eee0
Brian Groenke <brian@forerunnergames.com>
2015-12-21 21:58:29 -0500

    Add country occupation to core logic.
    Attack phase logic was missing a key element; namely, the country occupation upon defeat of a defending country. This commit adds PlayerOccupy* events to peril-common, as well as the necessary logic to GameModel and the state machine in order to query army occupation information from the attacking player.
    
    Other changes:
    - Change all on**Event methods in StateMachineEventHandler to 'onEvent' ... I finally decided that looks much more sane.
    - Minor style fixes (add 'final' to a few classes in the 'battle' package).
    - Add new keys to PlayerTurnDataCache.
    
    See PERIL-322

---
+++ common/src/main/java/com/forerunnergames/peril/common/net/events/client/request/response/PlayerOccupyCountryResponseRequestEvent.java
@@ -0,0 +1,36 @@
+package com.forerunnergames.peril.common.net.events.client.request.response;
+
+import com.forerunnergames.peril.common.net.events.server.request.PlayerOccupyCountryRequestEvent;
+import com.forerunnergames.tools.common.Arguments;
+import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
+import com.forerunnergames.tools.net.events.remote.origin.client.ResponseRequestEvent;
+import com.forerunnergames.tools.net.events.remote.origin.server.ServerRequestEvent;
+
+public class PlayerOccupyCountryResponseRequestEvent implements ResponseRequestEvent
+{
+  private final int deltaArmyCount;
+
+  public PlayerOccupyCountryResponseRequestEvent (final int deltaArmyCount)
+  {
+    Arguments.checkIsNotNegative (deltaArmyCount, "deltaArmyCount");
+
+    this.deltaArmyCount = deltaArmyCount;
+  }
+
+  public int getDeltaArmyCount ()
+  {
+    return deltaArmyCount;
+  }
+
+  @Override
+  public Class <? extends ServerRequestEvent> getRequestType ()
+  {
+    return PlayerOccupyCountryRequestEvent.class;
+  }
+
+  @RequiredForNetworkSerialization
+  private PlayerOccupyCountryResponseRequestEvent ()
+  {
+    deltaArmyCount = 0;
+  }
+}
