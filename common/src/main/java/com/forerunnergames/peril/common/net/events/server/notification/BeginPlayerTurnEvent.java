a3fe9611bf8018f27692c7edccbbf8b9758dc884
Aaron Mahan <aaron@forerunnergames.com>
2015-12-26 03:16:15 -0900

    Fix event code style & standard.
    - Make network serialization constructor private, not public.
    - Add toString method. Addresses PERIL-187.

--- common/src/main/java/com/forerunnergames/peril/common/net/events/server/notification/BeginPlayerTurnEvent.java
+++ common/src/main/java/com/forerunnergames/peril/common/net/events/server/notification/BeginPlayerTurnEvent.java
@@ -3,6 +3,7 @@ package com.forerunnergames.peril.common.net.events.server.notification;
 import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerNotificationEvent;
 import com.forerunnergames.peril.common.net.packets.person.PlayerPacket;
 import com.forerunnergames.tools.common.Arguments;
+import com.forerunnergames.tools.common.Strings;
 import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
 
 public class BeginPlayerTurnEvent implements PlayerNotificationEvent
@@ -22,8 +23,14 @@ public class BeginPlayerTurnEvent implements PlayerNotificationEvent
     return player;
   }
 
+  @Override
+  public String toString ()
+  {
+    return Strings.format ("{}: Player: {}", getClass ().getSimpleName (), player);
+  }
+
   @RequiredForNetworkSerialization
-  public BeginPlayerTurnEvent ()
+  private BeginPlayerTurnEvent ()
   {
     player = null;
   }
