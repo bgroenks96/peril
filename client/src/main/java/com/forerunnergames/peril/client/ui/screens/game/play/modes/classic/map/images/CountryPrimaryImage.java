782307e9177a7979eb32f737970bfa51aa821050
Aaron Mahan <aaron@forerunnergames.com>
2016-01-04 13:55:23 -0900

    Add missing @Nullable annotations.

--- client/src/main/java/com/forerunnergames/peril/client/ui/screens/game/play/modes/classic/map/images/CountryPrimaryImage.java
+++ client/src/main/java/com/forerunnergames/peril/client/ui/screens/game/play/modes/classic/map/images/CountryPrimaryImage.java
@@ -2,9 +2,11 @@ package com.forerunnergames.peril.client.ui.screens.game.play.modes.classic.map.
 
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
+import javax.annotation.Nullable;
+
 public final class CountryPrimaryImage extends AbstractCountryImage <CountryPrimaryImageState>
 {
-  public CountryPrimaryImage (final Drawable drawable,
+  public CountryPrimaryImage (@Nullable final Drawable drawable,
                               final String countryName,
                               final CountryImageState <CountryPrimaryImageState> state)
   {
--- client/src/main/java/com/forerunnergames/peril/client/ui/screens/game/play/modes/classic/map/images/CountrySecondaryImage.java
+++ client/src/main/java/com/forerunnergames/peril/client/ui/screens/game/play/modes/classic/map/images/CountrySecondaryImage.java
@@ -2,9 +2,11 @@ package com.forerunnergames.peril.client.ui.screens.game.play.modes.classic.map.
 
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
+import javax.annotation.Nullable;
+
 public final class CountrySecondaryImage extends AbstractCountryImage <CountrySecondaryImageState>
 {
-  public CountrySecondaryImage (final Drawable drawable,
+  public CountrySecondaryImage (@Nullable final Drawable drawable,
                                 final String countryName,
                                 final CountryImageState <CountrySecondaryImageState> state)
   {
