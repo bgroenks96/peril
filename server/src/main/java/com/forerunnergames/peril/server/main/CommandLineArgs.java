1816f79142015725962ce8b7c480373ec08df22e
Aaron Mahan <aaron@forerunnergames.com>
2016-01-05 16:09:38 -0900

    Fix command line arguments bug on server.
    - Fix NullPointerException's being thrown when running server from the
      command line, when omitting --players, --win-percent, or --assignment
      flags.
    
    - --players, --win-percent, & --assignment are all optional flags, but
      there is no default value set in case they are omitted, resulting in
      an early NullPointerException.
    
    - Make --players flag required. It should be required to specify the
      number of players in the game anyway. This eliminates the bug for this
      flag.
    
    - Assign a default value to --win-percent of 100. That is the only safe
      default value that could possibly be assigned. The only other option
      would be to make this flag required, but that seems like overkill.
    
    - Assign a default value to --assignment of
      InitialCountryAssignment#RANDOM. Making this flag required would also
      seem like overkill.

--- server/src/main/java/com/forerunnergames/peril/server/main/CommandLineArgs.java
+++ server/src/main/java/com/forerunnergames/peril/server/main/CommandLineArgs.java
@@ -10,35 +10,35 @@ import com.forerunnergames.peril.common.settings.NetworkSettings;
 public final class CommandLineArgs
 {
   @Parameter (names = { "--game-mode", "-g", }, description = "Game mode", required = true,
-          converter = GameModeParameterConverter.class, validateWith = GameModeParameterValidator.class)
+              converter = GameModeParameterConverter.class, validateWith = GameModeParameterValidator.class)
   public GameMode gameMode;
 
   @Parameter (names = { "--server-type", "-s" }, description = "Type of server", required = true,
-          converter = ServerTypeParameterConverter.class, validateWith = ServerTypeParameterValidator.class)
+              converter = ServerTypeParameterConverter.class, validateWith = ServerTypeParameterValidator.class)
   public GameServerType gameServerType;
 
   @Parameter (names = { "--title", "-t" }, description = "Server title", required = true,
-          validateWith = ServerTitleParameterValidator.class)
+              validateWith = ServerTitleParameterValidator.class)
   public String gameServerName;
 
   @Parameter (names = { "--map-name", "-m" }, description = "Map name", validateWith = MapNameParameterValidator.class,
-          required = true)
+              required = true)
   public String mapName;
+
+  @Parameter (names = { "--players", "-pl" }, description = "Maximum number of players allowed", required = true)
+  public Integer playerLimit;
 
   @Parameter (names = { "--port", "-p", }, description = "TCP port number")
   public Integer serverTcpPort = NetworkSettings.DEFAULT_TCP_PORT;
 
-  @Parameter (names = { "--players", "-pl" }, description = "Maximum number of players allowed")
-  public Integer playerLimit;
-
   @Parameter (names = { "--win-percent", "-w" },
-          description = "Minimum percentage of countries one must conquer to win the game")
-  public Integer winPercentage;
+              description = "Minimum percentage of countries one must conquer to win the game")
+  public Integer winPercentage = 100;
 
   @Parameter (names = { "--assignment", "-a" }, description = "Initial country assignment",
-          converter = InitialCountryAssignmentParameterConverter.class,
-          validateWith = InitialCountryAssignmentParameterValidator.class)
-  public InitialCountryAssignment initialCountryAssignment;
+              converter = InitialCountryAssignmentParameterConverter.class,
+              validateWith = InitialCountryAssignmentParameterValidator.class)
+  public InitialCountryAssignment initialCountryAssignment = InitialCountryAssignment.RANDOM;
 
   @Parameter (names = { "--help" }, help = true, description = "Show usage")
   public boolean help = false;
