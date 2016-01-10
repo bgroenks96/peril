bgroenks96/peril

commit b0b68f3b1c3eaf945ed9ec99b79d7222b386ade1

Implement player win/lose logic.
GameModel now checks the status of each player at the end of a turn phase for whether or not they are still in the game or have won. If a player has been eliminated, the player is removed from PlayerModel and a PlayerLoseGameEvent is sent. If a player has won, a PlayerWinGameEvent and internal EndGameEvent are sent.

server/src/main/java/com/forerunnergames/peril/server/controllers/MultiplayerController.java
@@ -20,6 +20,7 @@ import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerRespo
 import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerResponseSuccessEvent;
 import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerSuccessEvent;
 import com.forerunnergames.peril.common.net.events.server.notification.PlayerLeaveGameEvent;
+import com.forerunnergames.peril.common.net.events.server.notification.PlayerLoseGameEvent;
 import com.forerunnergames.peril.common.net.events.server.success.ChatMessageSuccessEvent;
 import com.forerunnergames.peril.common.net.events.server.success.JoinGameServerSuccessEvent;
 import com.forerunnergames.peril.common.net.events.server.success.ObserverJoinGameSuccessEvent;
@@ -262,8 +263,25 @@ public final class MultiplayerController extends ControllerAdapter
     }
     // remove client mapping
     remove (client.get ());
-    // send to everyone still in the server
-    sendToAllPlayersAndObservers (event);
+
+    // let handler for server notification events handle forwarding the event
+  }
+
+  @Handler
+  public void onEvent (final PlayerLoseGameEvent event)
+  {
+    final Optional <Remote> client = clientsToPlayers.clientFor (event.getPlayer ());
+    if (!client.isPresent ())
+    {
+      log.warn ("No client mapping for player in received event [{}].", event);
+      return;
+    }
+    // remove client/player mapping; keep client in server
+    clientsToPlayers.remove (client.get ());
+    // add client as an observer
+    clientsToObservers.put (client.get (), createNewObserverFromValidName (event.getPlayer ().getName ()));
+
+    // let handler for server notification events handle forwarding the event
   }
 
   @Handler
@@ -290,9 +308,6 @@ public final class MultiplayerController extends ControllerAdapter
   public void onEvent (final ServerNotificationEvent event)
   {
     Arguments.checkIsNotNull (event, "event");
-
-    // We have a separate handler / sender for this ServerNotificationEvent, so don't send twice.
-    if (event instanceof PlayerLeaveGameEvent) return;
 
     log.trace ("Event received [{}]", event);
 
@@ -509,14 +524,14 @@ public final class MultiplayerController extends ControllerAdapter
       return;
     }
 
-    final Result <ObserverJoinGameDeniedEvent.Reason> result = verifyObserverName (event.getObserverName ());
+    final Result <ObserverJoinGameDeniedEvent.Reason> result = validateObserverName (event.getObserverName ());
     if (result.failed ())
     {
       sendTo (client, new ObserverJoinGameDeniedEvent (event.getObserverName (), result.getFailureReason ()));
       return;
     }
 
-    final ObserverPacket observer = new DefaultObserverPacket (event.getObserverName (), UUID.randomUUID ());
+    final ObserverPacket observer = createNewObserverFromValidName (event.getObserverName ());
     clientsToObservers.put (client, observer);
 
     sendToAllPlayersAndObservers (new ObserverJoinGameSuccessEvent (observer));
@@ -702,7 +717,7 @@ public final class MultiplayerController extends ControllerAdapter
     return false;
   }
 
-  private Result <ObserverJoinGameDeniedEvent.Reason> verifyObserverName (final String name)
+  private Result <ObserverJoinGameDeniedEvent.Reason> validateObserverName (final String name)
   {
     if (clientsToPlayers.existsPlayerWith (name) || clientsToObservers.existsObserverWith (name))
     {
@@ -715,6 +730,12 @@ public final class MultiplayerController extends ControllerAdapter
     }
 
     return Result.success ();
+  }
+
+  // note: this method assumes that 'name' has already been validated
+  private ObserverPacket createNewObserverFromValidName (final String name)
+  {
+    return new DefaultObserverPacket (name, UUID.randomUUID ());
   }
 
   private void handlePlayerResponseTo (final Class <? extends ServerRequestEvent> requestClass,

common/src/main/java/com/forerunnergames/peril/common/game/rules/ClassicGameRules.java
@@ -20,6 +20,7 @@ public final class ClassicGameRules implements GameRules
   public static final int MAX_WIN_PERCENTAGE = 100;
   public static final int MIN_TOTAL_COUNTRY_COUNT = MAX_PLAYERS;
   public static final int MAX_TOTAL_COUNTRY_COUNT = 1000;
+  public static final int MIN_PLAYER_COUNTRY_COUNT = 1;
   public static final int MIN_ARMIES_IN_HAND = 0;
   public static final int MAX_ARMIES_IN_HAND = Integer.MAX_VALUE;
   public static final int MIN_ARMIES_ON_COUNTRY = 0;
@@ -61,6 +62,18 @@ public final class ClassicGameRules implements GameRules
   public InitialCountryAssignment getInitialCountryAssignment ()
   {
     return initialCountryAssignment;
+  }
+
+  @Override
+  public int getMinPlayerCountryCount ()
+  {
+    return MIN_PLAYER_COUNTRY_COUNT;
+  }
+
+  @Override
+  public int getMaxPlayerCountryCount ()
+  {
+    return getWinningCountryCount () - 1;
   }
 
   @Override

common/src/main/java/com/forerunnergames/peril/common/game/rules/GameRules.java
@@ -14,6 +14,10 @@ public interface GameRules
 
   InitialCountryAssignment getInitialCountryAssignment ();
 
+  int getMinPlayerCountryCount ();
+
+  int getMaxPlayerCountryCount ();
+
   int getMinArmiesInHand ();
 
   int getMaxArmiesInHand ();

common/src/main/java/com/forerunnergames/peril/common/net/events/server/notification/PlayerLoseGameEvent.java
@@ -1,0 +1,26 @@
+package com.forerunnergames.peril.common.net.events.server.notification;
+
+import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerNotificationEvent;
+import com.forerunnergames.peril.common.net.packets.person.PlayerPacket;
+import com.forerunnergames.tools.common.Arguments;
+
+/**
+ * An event reserved only for the biggest of failures and truest of scrubs.
+ */
+public final class PlayerLoseGameEvent implements PlayerNotificationEvent
+{
+  private final PlayerPacket player;
+
+  public PlayerLoseGameEvent (final PlayerPacket player)
+  {
+    Arguments.checkIsNotNull (player, "player");
+
+    this.player = player;
+  }
+
+  @Override
+  public PlayerPacket getPlayer ()
+  {
+    return player;
+  }
+}

common/src/main/java/com/forerunnergames/peril/common/net/events/server/notification/PlayerWinGameEvent.java
@@ -1,0 +1,26 @@
+package com.forerunnergames.peril.common.net.events.server.notification;
+
+import com.forerunnergames.peril.common.net.events.server.interfaces.PlayerNotificationEvent;
+import com.forerunnergames.peril.common.net.packets.person.PlayerPacket;
+import com.forerunnergames.tools.common.Arguments;
+
+/**
+ * An event reserved only for the biggest of failures and truest of scrubs.
+ */
+public final class PlayerWinGameEvent implements PlayerNotificationEvent
+{
+  private final PlayerPacket player;
+
+  public PlayerWinGameEvent (final PlayerPacket player)
+  {
+    Arguments.checkIsNotNull (player, "player");
+
+    this.player = player;
+  }
+
+  @Override
+  public PlayerPacket getPlayer ()
+  {
+    return player;
+  }
+}

core/src/main/java/com/forerunnergames/peril/core/model/GameModel.java
@@ -31,6 +31,8 @@ import com.forerunnergames.peril.common.net.events.server.notification.EndPlayer
 import com.forerunnergames.peril.common.net.events.server.notification.PlayerArmiesChangedEvent;
 import com.forerunnergames.peril.common.net.events.server.notification.PlayerCountryAssignmentCompleteEvent;
 import com.forerunnergames.peril.common.net.events.server.notification.PlayerLeaveGameEvent;
+import com.forerunnergames.peril.common.net.events.server.notification.PlayerLoseGameEvent;
+import com.forerunnergames.peril.common.net.events.server.notification.PlayerWinGameEvent;
 import com.forerunnergames.peril.common.net.events.server.request.PlayerAttackCountryRequestEvent;
 import com.forerunnergames.peril.common.net.events.server.request.PlayerDefendCountryRequestEvent;
 import com.forerunnergames.peril.common.net.events.server.request.PlayerFortifyCountryRequestEvent;
@@ -83,6 +85,7 @@ import com.forerunnergames.peril.core.model.state.StateTransitionAction;
 import com.forerunnergames.peril.core.model.state.annotations.StateMachineAction;
 import com.forerunnergames.peril.core.model.state.annotations.StateMachineCondition;
 import com.forerunnergames.peril.core.model.state.events.BeginManualCountrySelectionEvent;
+import com.forerunnergames.peril.core.model.state.events.EndGameEvent;
 import com.forerunnergames.peril.core.model.state.events.RandomlyAssignPlayerCountriesEvent;
 import com.forerunnergames.peril.core.model.turn.DefaultPlayerTurnModel;
 import com.forerunnergames.peril.core.model.turn.PlayerTurnModel;
@@ -212,6 +215,12 @@ public final class GameModel
   public void endPlayerTurn ()
   {
     log.info ("Turn ends for player [{}].", getCurrentPlayerPacket ().getName ());
+
+    // verify win/lose status of all players
+    for (final Id playerId : playerModel.getPlayerIds ())
+    {
+      checkPlayerGameStatus (playerId);
+    }
 
     playerTurnModel.advance ();
 
@@ -763,14 +772,17 @@ public final class GameModel
     }
     if (result.getDefendingCountryOwner ().isNot (defender.getPlayerId ()))
     {
-      final Id newOwner = countryOwnerModel.ownerOf (defender.getCountryId ());
-      final PlayerPacket newOwnerPlayer = playerModel.playerPacketWith (newOwner);
-      // publish country owner change notification
-      eventBus.publish (new CountryOwnerChangedEvent (defenderCountry,
-              playerModel.playerPacketWith (defender.getPlayerId ()), newOwnerPlayer));
+      final Id newOwnerId = countryOwnerModel.ownerOf (attacker.getCountryId ());
+      final Id prevOwnerId = countryOwnerModel.ownerOf (defender.getCountryId ());
+      final PlayerPacket prevOwner = playerModel.playerPacketWith (prevOwnerId);
+      final PlayerPacket newOwner = playerModel.playerPacketWith (newOwnerId);
+      final CountryPacket updatedDefenderCountry = countryMapGraphModel.countryPacketWith (defender.getCountryId ());
+      eventBus.publish (new CountryOwnerChangedEvent (updatedDefenderCountry, prevOwner, newOwner));
+
       // publish occupation request event (this must occur before attack success event in order for the
       // correct state transition to occur)
-      eventBus.publish (new PlayerOccupyCountryRequestEvent (newOwnerPlayer, attackerCountry, defenderCountry));
+      eventBus.publish (new PlayerOccupyCountryRequestEvent (newOwner,
+              countryMapGraphModel.countryPacketWith (attacker.getCountryId ()), updatedDefenderCountry));
     }
 
     clearCacheValues (CacheKey.BATTLE_ATTACKER_DATA, CacheKey.BATTLE_DEFENDER_DATA,
@@ -1019,6 +1031,26 @@ public final class GameModel
   {
     log.debug ("Turn: {} | Player: [{}] | Cache dump: [{}]", playerTurnModel.getTurn (), getCurrentPlayerId (),
                turnDataCache);
+  }
+
+  // checks whether or not a player has won or lost the game in the current game state
+  private void checkPlayerGameStatus (final Id playerId)
+  {
+    final int playerCountryCount = countryOwnerModel.countCountriesOwnedBy (playerId);
+    if (playerCountryCount < rules.getMinPlayerCountryCount ())
+    {
+      eventBus.publish (new PlayerLoseGameEvent (playerModel.playerPacketWith (playerId)));
+      playerModel.remove (playerId);
+      return;
+    }
+
+    if (playerCountryCount >= rules.getWinningCountryCount ())
+    {
+      // player won! huzzah!
+      eventBus.publish (new PlayerWinGameEvent (playerModel.playerPacketWith (playerId)));
+      // end the game
+      eventBus.publish (new EndGameEvent ());
+    }
   }
 
   /**

core/src/main/java/com/forerunnergames/peril/core/model/PlayerTurnDataCache.java
@@ -87,7 +87,7 @@ class PlayerTurnDataCache
   @Override
   public String toString ()
   {
-    return Strings.format ("{}: [{}]", dataCache);
+    return Strings.format ("{}: [{}]", getClass ().getSimpleName (), dataCache);
   }
 
   enum CacheKey

commit fe6c68ad4edfaaea910ecb2f0e6b80a1077caef7

Fix event handler ordering bug in MultiplayerController.
Fix heisenbug caused by b0b68f3b. PlayerLeaveGameEvent is now processed by both its handler AND the ServerNotificationEvent handler. This caused arbitrary selection of which handler to run first. The leave game event handler needs to be run first so that the player can be removed from the server before the notification is sent to all players.

- Add Integer.MIN_VALUE priority to ServerNotificationEvent handler so that any other handler for a notificiation event will run before it (it should always be run last since its doing the actual event publishing).

server/src/main/java/com/forerunnergames/peril/server/controllers/MultiplayerController.java
@@ -304,7 +304,7 @@ public final class MultiplayerController extends ControllerAdapter
     sendToPlayer (event.getPlayer (), event);
   }
 
-  @Handler
+  @Handler (priority = Integer.MIN_VALUE)
   public void onEvent (final ServerNotificationEvent event)
   {
     Arguments.checkIsNotNull (event, "event");