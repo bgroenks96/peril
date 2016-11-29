ed2c71aa3343a2162dad7e72a1d76d11402c10b7
Brian Groenke <brian@forerunnergames.com>
2016-11-29 14:10:36 -0500

    Add missing ! to IP address validation check.

--- server/src/main/java/com/forerunnergames/peril/server/controllers/MultiplayerController.java
+++ server/src/main/java/com/forerunnergames/peril/server/controllers/MultiplayerController.java
@@ -20,9 +20,6 @@ package com.forerunnergames.peril.server.controllers;
 
 import static com.forerunnergames.tools.net.events.EventFluency.clientFrom;
 
-import com.forerunnergames.peril.ai.events.AiDisconnectionEvent;
-import com.forerunnergames.peril.ai.net.AiClient;
-import com.forerunnergames.peril.ai.net.AiClientConfiguration;
 import com.forerunnergames.peril.common.game.GameConfiguration;
 import com.forerunnergames.peril.common.game.PersonLimits;
 import com.forerunnergames.peril.common.net.GameServerConfiguration;
@@ -74,14 +71,13 @@ import com.forerunnergames.tools.common.Strings;
 import com.forerunnergames.tools.common.controllers.ControllerAdapter;
 import com.forerunnergames.tools.net.NetworkConstants;
 import com.forerunnergames.tools.net.Remote;
+import com.forerunnergames.tools.net.client.ClientConfiguration;
 import com.forerunnergames.tools.net.client.ClientConnector;
-import com.forerunnergames.tools.net.client.configuration.ClientConfiguration;
-import com.forerunnergames.tools.net.client.configuration.DefaultClientConfiguration;
+import com.forerunnergames.tools.net.client.DefaultClientConfiguration;
 import com.forerunnergames.tools.net.events.local.ClientCommunicationEvent;
 import com.forerunnergames.tools.net.events.local.ClientConnectionEvent;
 import com.forerunnergames.tools.net.events.local.ClientDisconnectionEvent;
 import com.forerunnergames.tools.net.events.remote.origin.client.ResponseRequestEvent;
-import com.forerunnergames.tools.net.events.remote.origin.server.BroadcastEvent;
 import com.forerunnergames.tools.net.events.remote.origin.server.ServerRequestEvent;
 
 import com.google.common.base.Optional;
@@ -626,7 +622,7 @@ public final class MultiplayerController extends ControllerAdapter
     Arguments.checkIsNotNull (event, "event");
     Arguments.checkIsNotNull (client, "client");
 
-    if (NetworkConstants.isValidIpAddress (client.getAddress ()))
+    if (!NetworkConstants.isValidIpAddress (client.getAddress ()))
     {
       sendToClient (client, new PlayerRejoinGameDeniedEvent (PlayerRejoinGameDeniedEvent.Reason.INVALID_ADDRESS));
       return;
